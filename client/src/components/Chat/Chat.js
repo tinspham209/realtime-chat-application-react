6e6b5f1c5b01c9b70549d491deae01daf1729d8e
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-27 22:01:18 +0700

    refactor to deploy

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://react-chat-application-vxcvxc.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
