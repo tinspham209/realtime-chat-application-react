4fc8a01ad8d0607306ee8183603867ca2ce49465
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-27 16:09:57 +0700

    Add Messages component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import "./Chat.css";
 
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 let socket;
 
 const Chat = ({ location }) => {
@@ -48,6 +49,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
