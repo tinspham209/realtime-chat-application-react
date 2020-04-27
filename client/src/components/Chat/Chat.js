682520984d05f894b511048a17f72a3ffd8af0c9
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-27 15:46:41 +0700

    Add Input component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,7 @@ import io from "socket.io-client";
 import "./Chat.css";
 
 import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
 let socket;
 
 const Chat = ({ location }) => {
@@ -47,13 +48,11 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        {/* <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
-        /> */}
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
     </div>
   );
