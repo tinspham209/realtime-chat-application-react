30996c755f79db9767f6eebf79e26af20d3291ba
quay189 <34814999+quay189@users.noreply.github.com>
2020-04-27 16:12:25 +0700

    Create disconnect function

--- server/index.js
+++ server/index.js
@@ -41,7 +41,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("User had left!!!");
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left.`,
+      });
+    }
   });
 });
 
